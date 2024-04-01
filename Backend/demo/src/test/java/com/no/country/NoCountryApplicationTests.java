f5d078232137e091c17046f217ce4c2574899937
Bruno Sandoval <bruno.sandoval210@gmail.com>
2024-04-01 08:28:19 -0500

    Se subio el proyecto de spring

---
+++ Backend/demo/src/test/java/com/no/country/NoCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.no.country;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoCountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
