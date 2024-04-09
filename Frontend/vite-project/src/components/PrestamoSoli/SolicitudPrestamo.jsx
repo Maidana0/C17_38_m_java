6b7fed7b728693ae0bc6ce9c9a1aebd3b76a96b5
Maidana <franco_maidana07@hotmail.com>
2024-04-09 11:09:47 -0300

    - Probando ProgressBar

--- Frontend/vite-project/src/components/PrestamoSoli/SolicitudPrestamo.jsx
+++ Frontend/vite-project/src/components/PrestamoSoli/SolicitudPrestamo.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import "./prestamo.css";
 import "./selectorprestamo.css";
@@ -26,11 +26,11 @@ function FormularioPrestamo() {
   }, [amount]);
   const [selectedOption, setSelectedOption] = useState(null);
   const options = [
-    { id: 1, text: '12 cuotas', value: Math.round(amount/12) },
-    { id: 2, text: '6 cuotas', value:  Math.round(amount/6) },
-    { id: 3, text: '3 cuotas', value:  Math.round(amount/3)},
+    { id: 1, text: '12 cuotas', value: Math.round(amount / 12) },
+    { id: 2, text: '6 cuotas', value: Math.round(amount / 6) },
+    { id: 3, text: '3 cuotas', value: Math.round(amount / 3) },
   ];
-  
+
   const handleInputChange = (e) => {
     const inputtedValue = e.target.value.replace(/[^0-9]/g, ''); // Solo permite números
     setInputValue(e.target.value); // Actualiza inputValue con lo que el usuario escribe
@@ -40,7 +40,7 @@ function FormularioPrestamo() {
       // No actualizamos 'amount' si el campo está vacío
     } else {
       const numericValue = parseInt(inputtedValue, 10);
-      if (numericValue < min ) {
+      if (numericValue < min) {
         setErrorMessage(`El monto debe ser al menos $${min.toLocaleString()}.`);
       } else if (numericValue > max) {
         setErrorMessage(`El monto no debe exceder $${max.toLocaleString()}.`);
@@ -50,67 +50,67 @@ function FormularioPrestamo() {
       }
     }
   };
-  
+
   const handleSliderChange = (e) => {
     setAmount(Number(e.target.value));
     setErrorMessage(""); // Limpia el mensaje de error cuando se ajusta el slider
   };
-  const handleButtonClick = () => {
-    navigate('/Validate-Date'); // Usa el método navigate para cambiar de ruta
-  };
+  // const handleButtonClick = () => {
+  //   navigate('/Validate-Date'); // Usa el método navigate para cambiar de ruta
+  // };
   return (
     <div className="Prestamo">
-      
+
       <div className="loan-amount-slider">
-      <p className="p1">Elige el monto que necesitas, y elige una opción de pago.</p>
-      {errorMessage && <div className="error-message">{errorMessage}</div>}
-      <input
+        <p className="p1">Elige el monto que necesitas, y elige una opción de pago.</p>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+        <input
           type="text"
           className="amount-input"
           value={inputValue}
           onChange={handleInputChange}
-        />      
-      <div className="labels">
-        <span>${min.toLocaleString()}</span>
-        <input className="rangebar"
-        type="range" 
-        min={min} 
-        max={max} 
-        value={amount} 
-        onChange={handleSliderChange} 
-      />
-        <span>${max.toLocaleString()}</span>
-      </div>
-      
-        <p className="p2">Como preferis devolverlo?</p>
-      <div className="textos">
-        <p className="p3">Comenzá a pagarlo el {startDate}</p>
-      </div>
-      <div className="payment-options">
-      {options.map((option) => (
-        <div key={option.id} className="payment-option">
-          <input
-            type="radio"
-            id={`option-${option.id}`}
-            name="payment"
-            checked={selectedOption === option.id}
-            onChange={() => setSelectedOption(option.id)}
+        />
+        <div className="labels">
+          <span>${min.toLocaleString()}</span>
+          <input className="rangebar"
+            type="range"
+            min={min}
+            max={max}
+            value={amount}
+            onChange={handleSliderChange}
           />
-          <label htmlFor={`option-${option.id}`}>
-            <span className="option-text">{option.text}</span>
-            <span className="option-value">${option.value.toLocaleString()}</span>
-          </label>
+          <span>${max.toLocaleString()}</span>
         </div>
-      ))}
-    </div>
-      <div className="botonContinue"><button
-            onClick={handleButtonClick}>
-          
-            Continuar
-        </button></div>
-        
+
+        <p className="p2">Como preferis devolverlo?</p>
+        <div className="textos">
+          <p className="p3">Comenzá a pagarlo el {startDate}</p>
+        </div>
+        <div className="payment-options">
+          {options.map((option) => (
+            <div key={option.id} className="payment-option">
+              <input
+                type="radio"
+                id={`option-${option.id}`}
+                name="payment"
+                checked={selectedOption === option.id}
+                onChange={() => setSelectedOption(option.id)}
+              />
+              <label htmlFor={`option-${option.id}`}>
+                <span className="option-text">{option.text}</span>
+                <span className="option-value">${option.value.toLocaleString()}</span>
+              </label>
+            </div>
+          ))}
+        </div>
+        {/* <div className="botonContinue"><button
+          onClick={handleButtonClick}>
+
+          Continuar
+        </button></div> */}
+
       </div>
-          
+
     </div>
   );
 }
