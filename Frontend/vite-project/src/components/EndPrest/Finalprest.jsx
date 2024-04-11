297677a944e9f16fca4c5a58f857883ccce275b4
dqniel <danielsanabria12396@gmail.com>
2024-04-11 08:42:03 -0500

    Cambios prestamos

---
+++ Frontend/vite-project/src/components/EndPrest/Finalprest.jsx
@@ -0,0 +1,14 @@
+import "./okPrest.css"
+
+function Finalprest() {
+    return (
+      <div className="Finalprest-container">
+        <h1>¡Listo!</h1>
+        <h2>Confirmación</h2>
+        <p>Estamos trabajando para que tu préstamo se vea reflejado en tu cuenta bancaria en 24hs.</p>
+        <button className="inicio-button">Volver al Inicio</button>
+      </div>
+    );
+  }
+  
+  export default Finalprest;
--- Frontend/vite-project/src/pages/SolucitudPrestamo.jsx
+++ Frontend/vite-project/src/pages/SolucitudPrestamo.jsx
@@ -2,20 +2,30 @@ import { useState } from "react"
 import ProgressBar from "../components/progressBar/ProgressBar"
 import FormularioPrestamo from "../components/PrestamoSoli/SolicitudPrestamo"
 import ValidateDatePrest from "../components/ValidateDate/ValidateDatePrest"
-
+import ResumePrest from "../components/ConfirmPrest/ResumePrest";
+import Finalprest from "../components/EndPrest/Finalprest";
 const Solucitud= () => {
     const [step, setStep ] = useState(1)
+    const [amount, setAmount] = useState(70000);
+    const [selectedOption, setSelectedOption] = useState(null);
+    const [startDate, setStartDate] = useState('');
+    const options = [
+        { id: 1, text: '12 cuotas', value: Math.round(amount / 12) },
+        { id: 2, text: '6 cuotas', value: Math.round(amount / 6) },
+        { id: 3, text: '3 cuotas', value: Math.round(amount / 3) },
+      ];
+
+
   return (
     <div style={{backgroundColor:"#fff", marginTop:"3rem"}}>
-        <ProgressBar currentStep={step} totalSteps={4} setStep={setStep}>
+        <ProgressBar currentStep={step} totalSteps={4} setStep={setStep} >
 
         step = 1
         componente 1
         {
             step == 1 &&
-            <FormularioPrestamo />
+            <FormularioPrestamo amount={amount} setAmount={setAmount} options={options} selectedOption={selectedOption} setSelectedOption={setSelectedOption} startDate={startDate} setStartDate={setStartDate}/>
         }
-
 
         step = 2
         componente 2
@@ -27,10 +37,23 @@ const Solucitud= () => {
         step = 3
         componente 3
 
+        {
+            step == 3 &&
+            <ResumePrest amount={amount} setAmount={setAmount}  options={options} selectedOption={selectedOption} setSelectedOption={setSelectedOption} startDate={startDate} setStartDate={setStartDate}/>
+        
+        }
+
+        step = 4
+        componente 4 
+        {
+            step == 4 &&
+            <Finalprest/>
+        }
+
         </ProgressBar>
 
     </div>
   )
 }
 
-export default Solucitud
+export default Solucitud;
