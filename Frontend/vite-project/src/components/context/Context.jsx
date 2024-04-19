bda1b6a48bd9cffc2ddf6cbf8c014e8cded54695
Zanchez <szwallie26@gmail.com>
2024-04-19 13:15:38 -0300

    context restaurado

--- Frontend/vite-project/src/components/context/Context.jsx
+++ Frontend/vite-project/src/components/context/Context.jsx
@@ -5,8 +5,17 @@ export const Context = createContext();
 export const ContextProvider = ({ children }) => {
   const [estadoUsuario, setEstadoUsuario] = useState(0);
   const [estadoHeader, setEstadoHeader] = useState(0);
-  const [user, setUser] = useState(null);
+  const [userP, setUserP] = useState(null);
+  const [user, setUser] = useState(0);
   const [userCreated, setUserCreated] = useState();
+
+  function imagenMov ( valor ){
+    if (valor == "Préstamo") return "https://res.cloudinary.com/dabb8jxxh/image/upload/v1713313165/Cashfly/Group_52_wrc0rg.svg"
+    else if (valor == "Inversión") return "https://res.cloudinary.com/dabb8jxxh/image/upload/v1713313165/Cashfly/Group_53_vibqwt.svg"
+    else if (valor == "Depósito") return "https://res.cloudinary.com/dabb8jxxh/image/upload/v1713313165/Cashfly/Group_55_udxvbz.svg"
+    else if (valor == "Transferencia") return "https://res.cloudinary.com/dabb8jxxh/image/upload/v1713313170/Cashfly/Group_54_th3mhc.svg"
+    else return ""
+  }
 
   return (
     <Context.Provider
