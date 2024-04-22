a077c142d7cec17d1fb81a93ff2d33722cc8ff80
Maidana <franco_maidana07@hotmail.com>
2024-04-22 14:56:33 -0300

    - Inversiones (conexion con backend), pequeños ajustes en componentes de inversiones y barra de progreso

--- Frontend/vite-project/src/components/investment/StepOne.jsx
+++ Frontend/vite-project/src/components/investment/StepOne.jsx
@@ -1,13 +1,6 @@
 import { transformNumber, InputRange } from "./InputRange"
 
-const Info = ({ className, info }) => <div className={className}> <h5>{info.title}</h5>
-  <div><strong>$ {info.amount}</strong>  </div></div>
-
-
-
 const StepOne = ({ totalFunds, setTotal, styles }) => {
-
-
 
   const infoArray = [
     { title: "Disponible para invertir", amount: transformNumber(totalFunds) },
@@ -26,7 +19,16 @@ const StepOne = ({ totalFunds, setTotal, styles }) => {
       </div>
 
 
-      {infoArray.map((info, i) => <Info key={i} className={styles.info} info={info} />)}
+      {
+        infoArray.map((info, i) => (
+          <div key={i} className={styles.info}>
+            <h5>{info.title}</h5>
+            <div><strong>$ {info.amount}</strong>
+            </div>
+          </div>
+        )
+        )
+      }
 
 
 
